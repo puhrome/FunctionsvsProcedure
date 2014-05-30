ee5649521e18f69b3b07f3b16d39b27adcbcbb55
puhrome <phirom.yim@fullsail.edu>
2014-05-29 21:31:37 -0400

    FunctionsVsProcedures
    Explaining the difference between a function and procedure

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Functions vs Procedure Created by puhrome on 5/29/14.
+ */
+//Procedure is a function
+//Procedure doesn't return value
+
+
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
+}
+
+//this is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area);//no return
+}
